59ecb5da132b5360bee14e686fe6c3fd49e92fc0
Amit212 <amomnoya@gmail.com>
2024-07-15 22:13:59 +0300

    remove default kwargs

--- src/schema.py
+++ src/schema.py
@@ -2,12 +2,12 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 def get_player_schema():
     return StructType([
-        StructField("Name", StringType(), True),
-        StructField("Age", IntegerType(), True),
-        StructField("Nationality", StringType(), True),
-        StructField("Fifa Score", IntegerType(), True),
-        StructField("Club", StringType(), True),
-        StructField("Value", StringType(), True),
-        StructField("Salary", StringType(), True),
-        StructField("UpdatedAt", StringType(), True)
+        StructField("Name", StringType()),
+        StructField("Age", IntegerType()),
+        StructField("Nationality", StringType()),
+        StructField("Fifa Score", IntegerType()),
+        StructField("Club", StringType()),
+        StructField("Value", StringType()),
+        StructField("Salary", StringType()),
+        StructField("UpdatedAt", StringType())
     ])
